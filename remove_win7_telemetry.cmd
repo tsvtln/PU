125d41d18fab95f40930ef0acfd8e48284046c29
TSVTLN <112159858+tsvtln@users.noreply.github.com>
2023-10-17 14:18:42 +0300

    Create remove_win7_telemetry.cmd

---
+++ remove_win7_telemetry.cmd
@@ -0,0 +1,57 @@
+@echo off
+echo Uninstalling KB3075249 (telemetry for Win7/8.1)
+start /w wusa.exe /uninstall /kb:3075249 /quiet /norestart
+echo Uninstalling KB3080149 (telemetry for Win7/8.1)
+start /w wusa.exe /uninstall /kb:3080149 /quiet /norestart
+echo Uninstalling KB3021917 (telemetry for Win7)
+start /w wusa.exe /uninstall /kb:3021917 /quiet /norestart
+echo Uninstalling KB3022345 (telemetry)
+start /w wusa.exe /uninstall /kb:3022345 /quiet /norestart
+echo Uninstalling KB3068708 (telemetry)
+start /w wusa.exe /uninstall /kb:3068708 /quiet /norestart
+echo Uninstalling KB3044374 (Get Windows 10 for Win8.1)
+start /w wusa.exe /uninstall /kb:3044374 /quiet /norestart
+echo Uninstalling KB3035583 (Get Windows 10 for Win7sp1/8.1)
+start /w wusa.exe /uninstall /kb:3035583 /quiet /norestart
+echo Uninstalling KB2990214 (Get Windows 10 for Win7)
+start /w wusa.exe /uninstall /kb:2990214 /quiet /norestart
+echo Uninstalling KB2952664 (Get Windows 10 assistant)
+start /w wusa.exe /uninstall /kb:2952664 /quiet /norestart
+echo Uninstalling KB3075853 (update for "Windows Update" on Win8.1/Server 2012R2)
+start /w wusa.exe /uninstall /kb:3075853 /quiet /norestart
+echo Uninstalling KB3065987 (update for "Windows Update" on Win7/Server 2008R2)
+start /w wusa.exe /uninstall /kb:3065987 /quiet /norestart
+echo Uninstalling KB3050265 (update for "Windows Update" on Win7)
+start /w wusa.exe /uninstall /kb:3050265 /quiet /norestart
+echo Uninstalling KB971033  (license validation)
+start /w wusa.exe /uninstall /kb:971033 /quiet /norestart
+echo Uninstalling KB2902907 (description not available)
+start /w wusa.exe /uninstall /kb:2902907 /quiet /norestart
+echo Uninstalling KB2976987 (description not available)
+start /w wusa.exe /uninstall /kb:2976987 /quiet /norestart
+echo Uninstalling KB2976978 (compactibility update for Windows 8.1)
+start /w wusa.exe /uninstall /kb:2976978 /quiet /norestart
+echo Uninstalling KB3102810 (update for "Windows Update")
+start /w wusa.exe /uninstall /kb:3102810 /quiet /norestart
+echo Uninstalling KB3112343 (Windows Update Client for Windows 7)
+start /w wusa.exe /uninstall /kb:3112343 /quiet /norestart
+echo Uninstalling KB3135445 (Windows Update Client for Windows 7)
+start /w wusa.exe /uninstall /kb:3135445 /quiet /norestart
+echo Uninstalling KB3123862 (Windows Update Client for Windows 7)
+start /w wusa.exe /uninstall /kb:3123862 /quiet /norestart
+echo Uninstalling KB3081954 (Telemetry Update for Windows 7)
+start /w wusa.exe /uninstall /kb:3081954 /quiet /norestart
+echo Uninstalling KB3139929 (Get Windows 10 update for MSIE)
+start /w wusa.exe /uninstall /kb:3139929 /quiet /norestart
+echo Uninstalling KB3138612 (Windows Update Client for Windows 7)
+start /w wusa.exe /uninstall /kb:3138612 /quiet /norestart
+echo Uninstalling KB3138615 (Windows Update Client for Windows 8.1)
+start /w wusa.exe /uninstall /kb:3138615 /quiet /norestart
+echo Uninstalling KB3150513 (Compactibility Update (another GWX) for Windows 7/8.1)
+start /w wusa.exe /uninstall /kb:3150513 /quiet /norestart
+echo Uninstalling KB3133977 (buggy update)
+start /w wusa.exe /uninstall /kb:3133977 /quiet /norestart
+echo Uninstalling KB3139923 (Another GWX for Windows 7/8.1)
+start /w wusa.exe /uninstall /kb:3139923 /quiet /norestart
+echo Uninstalling KB3173040 (Another GWX for Windows 7/8.1)
+start /w wusa.exe /uninstall /kb:3173040 /quiet /norestart
