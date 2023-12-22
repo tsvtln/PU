fe18e5a4f126d5fde92f9e4e92fa4b1331d619ab
tsvtln <tsvtln@abv.bg>
2023-12-22 12:00:57 +0200

    Update cust_rez.sh

--- cust_rez.sh
+++ cust_rez.sh
@@ -1,12 +1,12 @@
-#xrandr --newmode "1368x768_60.00"   85.25  1368 1440 1576 1784  768 771 781 798 -hsync +vsync
-#xrandr --addmode VGA-1 1368x768_60.00
+# xrandr --newmode "1368x768_60.00"   85.25  1368 1440 1576 1784  768 771 781 798 -hsync +vsync
+# xrandr --addmode VGA-1 1368x768_60.00
 
 
-#  xrandr --newmode "1080x1920_60.00"  176.50  1080 1168 1280 1480  1920 1923 1933 1989 -hsync +vsync
-#  xrandr --addmode VGA-1 1080x1920_60.00
+# xrandr --newmode "1080x1920_60.00"  176.50  1080 1168 1280 1480  1920 1923 1933 1989 -hsync +vsync
+# xrandr --addmode VGA-1 1080x1920_60.00
 
-#  xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
-#  xrandr --addmode VGA-1 1920x1080_60.00
+# xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+# xrandr --addmode VGA-1 1920x1080_60.00
 
 xrandr --newmode "1600x900_60.00"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync
 xrandr --addmode VGA-1 1600x900_60.00
